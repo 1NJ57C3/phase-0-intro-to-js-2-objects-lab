ef66e0f915d447f8e49f87cd0c4a64686d5bd060
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2021-11-04 02:17:00 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "name",
+    streetAddress: "streetAddress",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // const newEmployee = { ...employee };
+    // newEmployee[key] = value;
+    // return newEmployee;
+    return {
+        ...employee,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeDelete = { ...employee };
+    delete employeeDelete[key];
+    return employeeDelete;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
